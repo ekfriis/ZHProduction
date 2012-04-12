7e5d6d0c452145af827ce4d6cdd41ba9f532dc4e
Evan Friis <evan.friis@cern.ch>
2012-04-12 08:12:32 -0500

    Add pythia generator cfgs

---
+++ CMSSW_4_2_8_patch4/src/PYTHIA6_Tauola_SM_H_2W_2lnu_zh_mH155_7TeV_cff_py_GEN.py
@@ -0,0 +1,171 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.303.2.7 
+# Source: /cvs/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: Configuration/Generator/python/PYTHIA6_Tauola_SM_H_2W_2lnu_zh_mH155_7TeV_cff.py -s GEN --conditions START42_V14B::All --eventcontent FEVT --datatier GEN-SIM-RAW --pileup NoPileUp -n 10 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('GEN')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic7TeV2011Collision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.0 $'),
+    annotation = cms.untracked.string('PYTHIA6 WH, H->WW->lnulnu mH=155 with TAUOLA at 7TeV'),
+    name = cms.untracked.string('$Source: Higgs request$')
+)
+
+# Output definition
+
+process.FEVToutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.FEVTEventContent.outputCommands,
+    fileName = cms.untracked.string('PYTHIA6_Tauola_SM_H_2W_2lnu_zh_mH155_7TeV_cff_py_GEN.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-RAW')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'START42_V14B::All'
+
+process.generator = cms.EDFilter("Pythia6GeneratorFilter",
+    ExternalDecays = cms.PSet(
+        Tauola = cms.untracked.PSet(
+            UseTauolaPolarization = cms.bool(True),
+            InputCards = cms.PSet(
+                mdtau = cms.int32(0),
+                pjak2 = cms.int32(0),
+                pjak1 = cms.int32(0)
+            )
+        ),
+        parameterSets = cms.vstring('Tauola')
+    ),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(1),
+    PythiaParameters = cms.PSet(
+        pythiaUESettings = cms.vstring('MSTU(21)=1     ! Check on possible errors during program execution', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'PARP(82)=1.832 ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1800. ! sqrts for which PARP82 is set', 
+            'PARP(90)=0.275 ! Multiple interactions: rescaling power', 
+            'MSTP(95)=6     ! CR (color reconnection parameters)', 
+            'PARP(77)=1.016 ! CR', 
+            'PARP(78)=0.538 ! CR', 
+            'PARP(80)=0.1   ! Prob. colored parton from BBR', 
+            'PARP(83)=0.356 ! Multiple interactions: matter distribution parameter', 
+            'PARP(84)=0.651 ! Multiple interactions: matter distribution parameter', 
+            'PARP(62)=1.025 ! ISR cutoff', 
+            'MSTP(91)=1     ! Gaussian primordial kT', 
+            'PARP(93)=10.0  ! primordial kT-max', 
+            'MSTP(81)=21    ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model'),
+        processParameters = cms.vstring('PMAS(25,1)=155       !mass of Higgs', 
+            'MSEL=0                  ! user selection for process', 
+            'MSUB(102)=0             !ggH', 
+            'MSUB(123)=0             !ZZ fusion to H', 
+            'MSUB(124)=0             !WW fusion to H', 
+            'MSUB(24)=1              !ZH production', 
+            'MSUB(26)=0              !WH production', 
+            'MSUB(121)=0             !gg to ttH', 
+            'MSUB(122)=0             !qq to ttH', 
+            'MDME(190,1) = 0            !W decay into dbar u', 
+            'MDME(191,1) = 0            !W decay into dbar c', 
+            'MDME(192,1) = 0            !W decay into dbar t', 
+            'MDME(194,1) = 0            !W decay into sbar u', 
+            'MDME(195,1) = 0            !W decay into sbar c', 
+            'MDME(196,1) = 0            !W decay into sbar t', 
+            'MDME(198,1) = 0            !W decay into bbar u', 
+            'MDME(199,1) = 0            !W decay into bbar c', 
+            'MDME(200,1) = 0            !W decay into bbar t', 
+            'MDME(206,1) = 1            !W decay into e+ nu_e', 
+            'MDME(207,1) = 1            !W decay into mu+ nu_mu', 
+            'MDME(208,1) = 1            !W decay into tau+ nu_tau', 
+            'MDME(174,1)=0     !Z decay into d dbar', 
+            'MDME(175,1)=0     !Z decay into u ubar', 
+            'MDME(176,1)=0     !Z decay into s sbar', 
+            'MDME(177,1)=0     !Z decay into c cbar', 
+            'MDME(178,1)=0     !Z decay into b bbar', 
+            'MDME(179,1)=0     !Z decay into t tbar', 
+            'MDME(182,1)=1     !Z decay into e- e+', 
+            'MDME(183,1)=0     !Z decay into nu_e nu_ebar', 
+            'MDME(184,1)=1     !Z decay into mu- mu+', 
+            'MDME(185,1)=0     !Z decay into nu_mu nu_mubar', 
+            'MDME(186,1)=1     !Z decay into tau- tau+', 
+            'MDME(187,1)=0     !Z decay into nu_tau nu_taubar', 
+            'MDME(210,1)=0           !Higgs decay into dd', 
+            'MDME(211,1)=0           !Higgs decay into uu', 
+            'MDME(212,1)=0           !Higgs decay into ss', 
+            'MDME(213,1)=0           !Higgs decay into cc', 
+            'MDME(214,1)=0           !Higgs decay into bb', 
+            'MDME(215,1)=0           !Higgs decay into tt', 
+            'MDME(216,1)=0           !Higgs decay into', 
+            'MDME(217,1)=0           !Higgs decay into Higgs decay', 
+            'MDME(218,1)=0           !Higgs decay into e nu e', 
+            'MDME(219,1)=0           !Higgs decay into mu nu mu', 
+            'MDME(220,1)=0           !Higgs decay into tau nu tau', 
+            'MDME(221,1)=0           !Higgs decay into Higgs decay', 
+            'MDME(222,1)=0           !Higgs decay into g g', 
+            'MDME(223,1)=0           !Higgs decay into gam gam', 
+            'MDME(224,1)=0           !Higgs decay into gam Z', 
+            'MDME(225,1)=0           !Higgs decay into Z Z', 
+            'MDME(226,1)=1           !Higgs decay into W W'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVToutput_step = cms.EndPath(process.FEVToutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.endjob_step,process.FEVToutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
