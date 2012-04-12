ff90df9c48469920bf89514099944a158680dc2f
Evan Friis <evan.friis@cern.ch>
2012-04-12 07:44:42 -0500

    Fixes to build into Configuration/Generator

--- make_cfgs.sh
+++ make_cfgs.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Generate WH->WWW->3LNu for different mass points
+# Generate ZH->ZWW and ZH->Ztautau cffs
+
+output=CMSSW_4_2_8_patch4/src/Configuration/Generator/python/
 
 for mass in `seq 110 5 160` 
 do
-  cat template.py | sed "s|HIGGSMASS|$mass|" > PYTHIA6_Tauola_SM_H_2W_2lnu_wh_mH${mass}_7TeV_cff.py
+  cat zh_tt_template.py | sed "s|HIGGSMASS|$mass|" > $output/PYTHIA6_Tauola_SM_H_2Tau_zh_mH${mass}_7TeV_cff.py
 done
 
 for mass in `seq 110 5 160` 
 do
-  cat zh_tt_template.py | sed "s|HIGGSMASS|$mass|" > PYTHIA6_Tauola_SM_H_2Tau_zh_mH${mass}_7TeV_cff.py
-done
-
-for mass in `seq 110 5 160` 
-do
-  cat zh_ww_template.py | sed "s|HIGGSMASS|$mass|" > PYTHIA6_Tauola_SM_H_2W_2lnu_zh_mH${mass}_7TeV_cff.py
+  cat zh_ww_template.py | sed "s|HIGGSMASS|$mass|" > $output/PYTHIA6_Tauola_SM_H_2W_2lnu_zh_mH${mass}_7TeV_cff.py
 done
