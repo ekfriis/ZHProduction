4487790324a79db5110c27e09ac4b56a4f817643
Evan Friis <evan.friis@cern.ch>
2012-04-12 10:34:03 -0500

    Add crab builder script

---
+++ CMSSW_4_2_8_patch4/src/build_crabs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+for mass in `seq 110 5 160`
+do
+  echo $mass
+  tt_psetname=PYTHIA6_Tauola_SM_H_2Tau_zh_mH110_7TeV_cff_py_GEN.py
+  cat crab_gen_template.cfg | sed "s|PSET_NAME|$tt_psetname|g" | sed "s|PUBLISH_NAME|ZH_ZToLL_HToTauTau_M-$mass-7TeV-pythia6|g" > ZH_HTT_${mass}_crab.cfg
+  ww_psetname=PYTHIA6_Tauola_SM_H_2W_2lnu_zh_mH110_7TeV_cff_py_GEN.py
+  cat crab_gen_template.cfg | sed "s|PSET_NAME|$ww_psetname|g"  | sed "s|PUBLISH_NAME|ZH_ZToLL_HToWW_WWTo2LNu-$mass-7TeV-pythia6|g" > ZH_HWW_${mass}_crab.cfg
+done
+
