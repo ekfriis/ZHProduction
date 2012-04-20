8148958d81e759e15b091413b5f34de70272a1af
Evan Friis <evan.friis@cern.ch>
2012-04-20 08:50:27 -0500

    Add tool to build crab cfgs

---
+++ CMSSW_4_2_9_HLT1_patch1/src/DIGI_L1_DIGI2RAW_HLT:GRun_ALL_PU.py
@@ -0,0 +1,91 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.303.2.7 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: DIGI,L1,DIGI2RAW,HLT:GRun --filein dummy.root --conditions START42_V14B::All --pileup mix_E7TeV_Fall2011_Reprocess_50ns_PoissonOOTPU_cfi --datamix NODATAMIXER --eventcontent RAWSIM --datatier GEN-SIM-RAW -n 10 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('ALL')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mix_E7TeV_Fall2011_Reprocess_50ns_PoissonOOTPU_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic7TeV2011Collision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.PostRecoGenerator_cff')
+process.load('Configuration.StandardSequences.Validation_cff')
+process.load('DQMOffline.Configuration.DQMOfflineMC_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('dummy.root')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.303.2.7 $'),
+    annotation = cms.untracked.string('DIGI,L1,DIGI2RAW,HLT:GRun nevts:10'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('DIGI_L1_DIGI2RAW_HLT:GRun_ALL_PU.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-RAW')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'START42_V14B::All'
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.postreco_step = cms.Path(process.postreco_generator)
+process.prevalidation_step = cms.Path(process.prevalidation)
+process.dqmoffline_step = cms.Path(process.DQMOffline)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.validation_step = cms.EndPath(process.validation)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.digitisation_step,process.L1simulation_step,process.digi2raw_step,process.raw2digi_step,process.reconstruction_step,process.postreco_step,process.prevalidation_step,process.validation_step,process.dqmoffline_step,process.endjob_step,process.RAWSIMoutput_step)
---
+++ CMSSW_4_2_9_HLT1_patch1/src/make_driver_command.sh
@@ -0,0 +1,3 @@
+cmsDriver.py DIGI,L1,DIGI2RAW,HLT:GRun --conditions START42_V14B::All --pileup \
+  mix_E7TeV_Fall2011_Reprocess_50ns_PoissonOOTPU_cfi --datamix NODATAMIXER \
+  --eventcontent RAWSIM --datatier GEN-SIM-RAW -n 10 --no_exec
