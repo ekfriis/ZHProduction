73d014329f713b8c3c58acc6a70191d4ad071f41
Evan Friis <evan.friis@cern.ch>
2012-04-20 08:55:55 -0500

    General CRAB nonsense

--- CMSSW_4_2_9_HLT1_patch1/src/generate_multicrab.sh
+++ CMSSW_4_2_9_HLT1_patch1/src/generate_multicrab.sh
@@ -9,12 +9,12 @@ echo "" >> multicrab.cfg
 
 for mass in `seq 110 5 160`
 do
-  echo "[WW_$mass]" >> multicrab.cfg
+  echo "[crab_digi_WW_$mass]" >> multicrab.cfg
   echo "CMSSW.datasetpath=/ZH_ZToLL_HToWW_WWTo2LNu-$mass-7TeV-pythia6/friis-ZH_ZToLL_HToWW_WWTo2LNu-$mass-7TeV-pythia6-6ce0bad598f569ccbf493e88b5e333e7/USER" >> multicrab.cfg
   echo "USER.publish_data_name=ZH_ZToLL_HToWW_WWTo2LNu-$mass-7TeV-pythia6" >> multicrab.cfg
   echo "" >> multicrab.cfg
 
-  echo "[TT_$mass]" >> multicrab.cfg
+  echo "[crab_digi_TT_$mass]" >> multicrab.cfg
   echo "CMSSW.datasetpath=/ZH_ZToLL_HToTauTau_M-$mass-7TeV-pythia6/friis-ZH_ZToLL_HToTauTau_M-$mass-7TeV-pythia6-a80cb94964a5eab22807f871801e3473/USER" >> multicrab.cfg
   echo "USER.publish_data_name=ZH_ZToLL_HToTauTau_M-$mass-7TeV-pythia6" >> multicrab.cfg
   echo "" >> multicrab.cfg
