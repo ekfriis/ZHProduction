fcdb1924c1f854cb72936e4c908f9e0210259787
Evan Friis <evan.friis@cern.ch>
2012-04-28 02:12:11 -0500

    Fixes to multicrab generation

--- CMSSW_4_2_8_patch4/src/generate_multicrab.sh
+++ CMSSW_4_2_8_patch4/src/generate_multicrab.sh
@@ -10,11 +10,11 @@ echo "" >> multicrab.cfg
 for mass in `seq 110 5 160`
 do
   # NOTE THAT EVEN THOUGH HTOTAU IS IN DATASET NAME ITS OKAY, CRAB JUST SUCKS
-  echo "[crab_digi_WW_$mass]" >> multicrab.cfg
-  echo "CMSSW.datasetpath=/ZH_ZToLL_HToWW_WWTo2LNu-160-7TeV-pythia6/friis-ZH_ZToLL_HToTauTau_M-$mass-7TeV-pythia6_crab_digi_WW_160-b94ed3ce6183d233f3644e7c7592cbfd/USER" >> multicrab.cfg
+  echo "[crab_reco_WW_$mass]" >> multicrab.cfg
+  echo "CMSSW.datasetpath=/ZH_ZToLL_HToWW_WWTo2LNu-$mass-7TeV-pythia6/friis-ZH_ZToLL_HToTauTau_M-$mass-7TeV-pythia6_crab_digi_WW_$mass-b94ed3ce6183d233f3644e7c7592cbfd/USER" >> multicrab.cfg
   echo "" >> multicrab.cfg
 
-  echo "[crab_digi_TT_$mass]" >> multicrab.cfg
+  echo "[crab_reco_TT_$mass]" >> multicrab.cfg
   echo "CMSSW.datasetpath=/ZH_ZToLL_HToTauTau_M-$mass-7TeV-pythia6/friis-ZH_ZToLL_HToTauTau_M-$mass-7TeV-pythia6_crab_digi_TT_$mass-b94ed3ce6183d233f3644e7c7592cbfd/USER" >> multicrab.cfg
   echo "" >> multicrab.cfg
 done
